45ae2d395a7f9b2d77076589a2527b5a0c0703e7
diego.quijano-at-202227537182 <dfqz93@hotmail.com>
2023-05-11 14:40:49 -0500

    module

---
+++ module.mjs
@@ -0,0 +1,15 @@
+import {PlatziClass} from "./class.mjs"
+
+const clase67 = new PlatziClass({
+    name: "Javascript: Orientado a objetos, basados en prototipos",
+    videoID: "pjlhkgfhgccghvjbijok",
+});
+
+clase67.reproducir();
+clase67.pausar();
+
+clase67.reproducir();
+clase67.pausar();
+
+clase67.reproducir();
+clase67.pausar();
