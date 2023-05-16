4c094ea635874902ef042de7f94e50e6a101ad66
diego.quijano-at-202227537182 <dfqz93@hotmail.com>
2023-05-16 12:51:04 -0500

    methods

--- intermediate/main.js
+++ intermediate/main.js
@@ -10,14 +10,39 @@ const diego = {
     }
 };
 
-console.log(Object.keys(diego));
-console.log(Object.getOwnPropertyNames(diego));
-console.log(Object.entries(diego));
-console.log(Object.getOwnPropertyDescriptors(diego));
+Object.defineProperty(diego, 'navigator', {
+    value: 'Chrome',
+    enumerable: false,
+    writable: true,
+    configurable: true,
+});
+
+Object.defineProperty(diego, 'editor', {
+    value: 'VSCode',
+    enumerable: true,
+    writable: false,
+    configurable: true,
+});
+
+Object.defineProperty(diego, 'terminal', {
+    value: 'WSL',
+    enumerable: true,
+    writable: true,
+    configurable: false,
+});
 
 Object.defineProperty(diego, 'pruebaNASA', {
     value: 'extraterrestres',
-    enumerable: true,
-    writable: true,
-    configurable: true,
-})
+    enumerable: false,
+    writable: false,
+    configurable: false,
+});
+
+console.log(Object.keys(diego));
+console.log(Object.getOwnPropertyNames(diego));
+console.log(Object.entries(diego));
+
+Object.seal(diego);
+Object.freeze(diego);
+
+console.log(Object.getOwnPropertyDescriptors(diego));
